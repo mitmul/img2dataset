2f1a056b8e56fe78e6051249281f730d7fef9eba
Shunta Saito <shunta.saito@gmail.com>
2023-08-21 01:41:14 +0000

    Fix

--- img2dataset/writer.py
+++ img2dataset/writer.py
@@ -141,14 +141,14 @@ class WebDatasetSampleWriter:
                 tar_upload_path = self.shard_name + ".tar"
                 print(f"Uplaoding {self.tmp_tar_path} to {tar_upload_path}...")
                 with fs.open(tar_upload_path, "wb") as out_f:
-                    with open(self.tmp_tar_path, "wb") as in_f:
+                    with open(self.tmp_tar_path, "rb") as in_f:
                         out_f.write(in_f.read())
         self.buffered_parquet_writer.write(meta)
         with pfio.v2.from_url(self.upload_path) as fs:
             parquet_upload_path = self.shard_name + ".parquet"
             print(f"Uploading {self.tmp_parquet_path} to {parquet_upload_path}...")
             with fs.open(parquet_upload_path, "wb") as out_f:
-                with open(self.tmp_parquet_path, "wb") as in_f:
+                with open(self.tmp_parquet_path, "rb") as in_f:
                     out_f.write(in_f.read())
 
     def close(self):
